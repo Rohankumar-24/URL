56906b8a8063f7a19e58f09d280af650d7ad0496
Rohan Kumar <rohanbhagat6806@gmail.com>
2025-09-17 00:28:01 +0530

    add mongo atlas

--- connection.js
+++ connection.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
 
-async function connectToMongoDB(url) {
-  return mongoose.connect(url);
+async function connectToMongoDB() {
+  return mongoose.connect(
+    "mongodb+srv://rohanbhagat6806_db_user:oAztbY9DjuQkjTOd@cluster0.759wxnu.mongodb.net/short-url",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
 }
 
 module.exports = {
